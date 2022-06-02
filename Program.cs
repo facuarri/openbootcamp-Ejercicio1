e3e5e801290dd02e51411a0a5195011c474fe7fd
facuarri <77691092+facuarri@users.noreply.github.com>
2022-06-01 21:29:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// C# - Ejercicios sesion 1
+
+// Ejercicio 1
+
+Console.WriteLine("Escribe tu nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre + ".");
+
+// Ejercicio 2
+Console.Write("La hora actual es: ");
+DateTime now = DateTime.Now;
+Console.WriteLine(now.Hour + ":" + now.Minute);
